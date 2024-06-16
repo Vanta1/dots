9f9c76927752370466d47ffd503484e44434d71a
Vanta_1 <mcoopersandys@gmail.com>
2024-06-15 21:13:24 -0400

    updated vscode

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -2,11 +2,11 @@
   programs.vscode = {
     enable = true;
     package = (pkgs.vscode.overrideAttrs (oldAttrs: rec {
-      version = "1.89.1";
+      version = "1.90.1";
       src = builtins.fetchurl {
         name = "VSCode_${version}_linux-x64.tar.gz";
         url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
-        sha256 = "1zfh48g6prjhjcyrz5impsnm6khw7s75k8k54bp0cszl81ddsysx";
+        sha256 = "039yb1v4vcgsyp3gfvsfm7pxivf20ycyvidhrk26jfm54ghbbnlz";
       };
     }));
   };

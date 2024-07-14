fa11338640abb32f317397bd28fb1d52720e21d3
Vanta_1 <mcoopersandys@gmail.com>
2024-07-14 16:28:39 -0400

    basic hyprlock config

--- home/programs/hyprland/hyprlock.nix
+++ home/programs/hyprland/hyprlock.nix
@@ -1,10 +1,17 @@
-{ inputs, config, pkgs, ... }: {
+{ inputs, config, pkgs, user, ... }: {
     programs.hyprlock = {
         enable = true;
-        #settings = {
-        #    background = {
-        #        monitor = 
-        #    }
-        #};
+        settings = {
+            general = {
+                disable_loading_bar = true;
+                hide_cursor = true;
+                no_fade_in = true;
+            };
+            background = {
+                monitor = ""; # leave empty for all monitors
+                path = "/home/${user}/wallpapers/mist_forest_2.png";
+                blur_passes = 2;
+            };
+        };
     };
 }

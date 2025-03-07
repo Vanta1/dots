4e3a293d87281de44468bc4701b3041a6f7e53e2
Vanta_1 <mcoopersandys@gmail.com>
2025-03-07 18:38:52 -0500

    updated dunst and vscode

--- home/programs/dunst.nix
+++ home/programs/dunst.nix
@@ -4,25 +4,38 @@
   xdg.configFile."dunst/dunstrc" = {
     text = ''
       [colors]
-      	background = "#232e33"
-      	foreground = "#d3c6aa"
+      background = "#232e33"
+      foreground = "#d3c6aa"
 
       [global]
-      	monitor = 1
-      	width = 400
-      	height = 300
-      	padding = 12
-      	horizontal_padding = 12
-      	frame_width = 3
-      	frame_color = "#2e383c"
-      	corner_radius = 12
-      	origin = bottom-right
-      	offset = 12x12
-      	gap_size = 12
-      	min_icon_size = 600
-      	font = Monaspace Xenon 12
-      	format = "<b>%a</b>\n%s\n%b"
-      	browser = ${pkgs.firefox}/bin/firefox
+      monitor = 1
+      width = 400
+      height = 300
+
+      padding = 12
+      text_icon_padding = 12
+      horizontal_padding = 12
+
+      frame_width = 3
+      frame_color = "#2e383c"
+
+      origin = bottom-right
+      offset = 12x12
+
+      gap_size = 12
+
+      corner_radius = 12
+      icon_corner_radius = 9
+
+      icon_theme = "Vimix"
+      icon_position = left
+      min_icon_size = 96
+      max_icon_size = 96
+
+      font = Monaspace Xenon 12
+      line_height = 2
+      format = "<b>%a</b>\n%s\n%b"
+      browser = ${pkgs.firefox}/bin/firefox
     '';
   };
 }
--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -4,13 +4,14 @@
     package = pkgs.vscode.overrideAttrs (oldAttrs: rec {
       # update this whenever vscode bugs you about an update. this way i can keep ahead of nixpkgs
       # you can find the latest version number here: https://code.visualstudio.com/updates
-      version = "1.97.1";
+      # or by clicking on "Release Notes" in the update notif
+      version = "1.98.0";
       src = builtins.fetchurl {
         name = "VSCode_${version}_linux-x64.tar.gz";
         url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
         # when the version number is changed the sha256 hash will be out of date.
         # just copy the "got" key from the error message and replace this field.
-        sha256 = "0gr2z4vzms6fv4kcc8dzc7l3inpb5hasnzdfr1zc2n4i3nl8z8vw";
+        sha256 = "1yywjdpdv2y71mdja7pzfj15vdrv1wj6r8k7a8n8sldk1blv0d1s";
       };
     });
   };

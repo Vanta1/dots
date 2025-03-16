8e8cce739f0ce8bcb020400de7d61322d9625966
Vanta_1 <mcoopersandys@gmail.com>
2025-03-16 14:59:15 -0400

    fixed firefox new tab

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -6,6 +6,10 @@
         "layout.css.devPixelsPerPx" = "1.3"; # rescale the ui to be a little larger, feels better on a small laptop
         "browser.tabs.closeWindowWithLastTab" = false; # i like to spam Ctrl+W to close all my tabs
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # enable userChrome.css
+
+        # remove firefox logo from new tabs
+        "browser.newtabpage.activity-stream.newtabLayouts.variant-a" = false;
+        "browser.newtabpage.activity-stream.newtabLayouts.variant-b" = false;
       };
       userChrome = builtins.readFile ./userChrome.css; # custom css styles
     };
